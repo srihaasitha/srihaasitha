deb8c4caee7685a1067546b320d527341d619937
srihaasitha <58470283+srihaasitha@users.noreply.github.com>
2019-12-03 20:11:58 +0530

    Add files via upload
    Command Line Argument programs

---
+++ p1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class p1
+{
+public static void main(String args[])
+{
+System.out.println(args[0]+" Technologies "+args[1]);
+}
+}
---
+++ p2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class p2
+{
+public static void main(String args[])
+{
+System.out.println("Welcome "+args[0]);
+}
+}
